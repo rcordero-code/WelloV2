bc121904898a657874d2ca56c4d7ab176613ed57
rcordero-code <rcordero@rivieraridge.org>
2026-03-11 15:11:00 -0700

    Add files via upload

---
+++ health.js
@@ -0,0 +1,37 @@
+const ALLOWED_ORIGINS = new Set([
+  'https://rcordero-code.github.io',
+  'http://localhost:5500',
+  'http://127.0.0.1:5500',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000'
+]);
+
+function applyCors(req, res) {
+  const origin = req.headers.origin;
+  if (origin && ALLOWED_ORIGINS.has(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+}
+
+export default function handler(req, res) {
+  applyCors(req, res);
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method not allowed' });
+    return;
+  }
+
+  res.status(200).json({
+    ok: true,
+    service: 'wello-api',
+    openaiConfigured: Boolean(process.env.OPENAI_API_KEY),
+    elevenLabsConfigured: Boolean(process.env.ELEVENLABS_API_KEY)
+  });
+}
